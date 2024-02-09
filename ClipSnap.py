60cbba03949b79b8f6d92644f55f93a6fce127ec
WireBits <159493381+wirebits@users.noreply.github.com>
2024-02-09 12:32:29 +0530

    Add files via upload

---
+++ ClipSnap.py
@@ -0,0 +1,29 @@
+#ClipSnap
+#A tool which records the copied images of the clipboard.
+#Author - Wirebits
+
+import os
+import sys
+import time
+from PIL import ImageGrab
+from datetime import datetime
+
+folder_name = "Clipboard Images"
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+image_count = 1
+
+while True:
+    current_time = datetime.now()
+    clipboard_image = ImageGrab.grabclipboard()
+    timestamp = current_time.strftime("%d-%m-%y_%H_%M_%S")
+    
+    if clipboard_image:
+        image_name = os.path.join(folder_name, f"image{image_count}_{timestamp}.png")
+        clipboard_image.save(image_name)
+        print(f"Image saved: {image_name}")
+        image_count += 1
+    else:
+        sys.exit()
+    time.sleep(1)
